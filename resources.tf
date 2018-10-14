858c17351c1a5207e681e09dc797103a3e68095e
Jay Gehlot <Jay.Gehlot@concur.com>
2018-10-14 22:45:48 +0100

    Add subnets to the VPC and CIDR block ranges for the subnets. Add a security group for the VPC also.

--- resources.tf
+++ resources.tf
@@ -6,3 +6,33 @@ resource "aws_vpc" "environment-example-one" {
     Name = "terraform-aws-vpc-example-one"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-one.cidr_block,3,1 )}"
+  # subnet cidr block points to the main vpc and defines a cidr block range for the subnet
+  vpc_id = "${aws_vpc.environment-example-one.id}"
+  #vpc id is automatically injected if there is only one vpc
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-one.cidr_block,2,2)}"
+  vpc_id = "${aws_vpc.environment-example-one.id}"
+  availability_zone = "us-west-2b"
+}
+
+#the security group is for the subnets, but its applied to the VPC
+resource "aws_security_group" "subnetsecuritygroup" {
+  vpc_id = "${aws_vpc.environment-example-one.id}"
+
+
+  ingress {
+    cidr_blocks = [
+     "${aws_vpc.environment-example-one.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
